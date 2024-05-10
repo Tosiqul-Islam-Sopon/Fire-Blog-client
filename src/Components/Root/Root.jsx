f83c955b4cbce7602ab11fe4d66b2c3a8a85de28
Tosiqul Islam <sopon.tosiq@gmail.com>
2024-05-10 10:32:58 +0600

    error page done

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,9 +1,11 @@
-import Home from "../Home/Home";
+import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
 
 const Root = () => {
     return (
         <div>
-            <Home></Home>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,13 @@ import {
 } from "react-router-dom";
 import Root from './Components/Root/Root';
 import Home from './Components/Home/Home';
+import ErrorPage from './Components/Error Page/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
-    // errorElement: <ErrorPage />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
