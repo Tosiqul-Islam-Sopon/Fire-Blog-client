40398d769101c88cb90771a2f9e9b24bef859ee8
Tosiqul Islam <sopon.tosiq@gmail.com>
2024-05-15 00:56:06 +0600

    deployed to firebase and replace localhost link

--- src/Components/Hooks/useAxiosBase.jsx
+++ src/Components/Hooks/useAxiosBase.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://fire-blog-server.vercel.app',
     withCredentials: true
 })
 
